24012f984323d47a30819e602f51a665046b5182
WillMcBurney <pmcburn1@gmail.com>
2024-03-15 10:32:16 -0400

    Initial commit

---
+++ lib/screens/log_in_screen.dart
@@ -0,0 +1,113 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_demo/screens/notes_screen.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key, required this.title});
+  final String title;
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  int _counter = 0;
+  final _emailFieldController = TextEditingController();
+  final _passwordFieldController = TextEditingController();
+  String _statusLabel = "";
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _signIn() async {
+    var result = "";
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailFieldController.text,
+        password: _passwordFieldController.text,
+      );
+      result = "Logged In - ${credential.user?.email}";
+      if (!mounted) return;
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const NotesScreen())
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        result = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        result = 'Wrong password provided for that user.';
+      }
+    } catch (e) {
+      result = e.toString();
+    }
+    setState(() {
+      _statusLabel = result;
+    });
+  }
+
+  void _createUser() async {
+    var result = "";
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailFieldController.text,
+        password: _passwordFieldController.text,
+      );
+      result = "User Created!";
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        result = 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        result = 'The account already exists for that email.';
+      }
+    } catch (e) {
+      result = e.toString();
+    }
+    setState(() {
+      _statusLabel = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Email"
+              ),
+              controller: _emailFieldController,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Password"
+              ),
+              obscureText: true,
+              controller: _passwordFieldController,
+            ),
+            TextButton(onPressed: _signIn, child: const Text("Sign-in")),
+            TextButton(onPressed: _createUser, child: const Text("Create User")),
+            Text(_statusLabel),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/screens/notes_screen.dart
@@ -0,0 +1,140 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class NotesScreen extends StatefulWidget {
+  const NotesScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  late User _user;
+  final _newNoteController = TextEditingController();
+  bool _userNotesOnlySwitch = false;
+
+  @override
+  void initState() {
+    _user = FirebaseAuth.instance.currentUser!;
+  }
+
+  Stream<QuerySnapshot> _allNotes() {
+    return FirebaseFirestore.instance.collection("notes").snapshots();
+  }
+
+  Stream<QuerySnapshot> _userNotes() {
+    return FirebaseFirestore.instance.collection("notes")
+        .where("author", isEqualTo: _user.email)
+        .snapshots();
+  }
+
+  void _saveNote() async {
+    var noteText = _newNoteController.text;
+    FirebaseFirestore.instance.collection("notes")
+      .add({
+        "note": noteText,
+        "votes": 0,
+        "author": _user.email,
+      }
+    ).then((value) {
+      var snackBar = const SnackBar(
+        content: Text("Note Added!"),
+        duration: Duration(seconds: 3),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Signed in!"),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Text("Welcome, ${_user.email}"),
+            TextField(
+              controller: _newNoteController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "New Note"
+              )
+            ),
+            TextButton(
+                onPressed: _saveNote,
+                child: const Text("Save")
+            ),
+            Switch(
+                value: _userNotesOnlySwitch,
+                onChanged: (bool value) {
+                  setState(() {
+                    _userNotesOnlySwitch = value;
+                  });
+                }
+            ),
+            firestoreNotesList(),
+          ]
+        ),
+      ),
+    );
+  }
+
+  Widget firestoreNotesList() {
+    return StreamBuilder(
+        stream: _userNotesOnlySwitch? _userNotes() : _allNotes(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong!");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
+          return ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) =>
+                noteView(snapshot.data!.docs[index])
+          );
+        } //builder
+    );
+  }
+
+  Widget noteView(DocumentSnapshot doc) {
+    return Row(
+      children: [
+        Text(doc["note"]),
+        const VerticalDivider(thickness: 15),
+        Text(doc["votes"].toString()),
+        const VerticalDivider(thickness: 15),
+        IconButton(
+            onPressed: () => _incrementVote(doc),
+            icon: const Icon(Icons.thumb_up)
+        ),
+        if (_user.email == doc["author"])
+          IconButton(
+              onPressed: () => _deleteNote(doc),
+              icon: const Icon(Icons.delete_outline)
+          ),
+      ],
+    );
+  }
+
+  void _deleteNote(DocumentSnapshot doc) {
+    FirebaseFirestore.instance.collection("notes").doc(doc.id).delete();
+  }
+
+  void _incrementVote(DocumentSnapshot doc) {
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot freshSnap = await transaction.get(doc.reference);
+      await transaction.update(freshSnap.reference, {
+        "votes": freshSnap["votes"] + 1
+      });
+    });
+  }
+}
